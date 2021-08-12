c6d693df70d47e0365e58f1194eed4cc8747bcef
Darren Ng <darren.ng@versed.ai>
2021-08-12 23:20:29 +0100

    added BinomialTree class for pricing american options

--- binomial.py
+++ binomial.py
@@ -6,14 +6,27 @@ class Option(object):
     """
     Stores common attributes of a stock option
     """
-    def __init__(self, S0, strike, r=0.05, T=1, N=2, pu=0, pd=0, div=0, sigma=0, is_put=False, is_american=False):
+    def __init__(
+        self,
+        S0: int,
+        K: int,
+        r=0.05,
+        T=1,
+        N=2,
+        pu=0,
+        pd=0,
+        div=0,
+        sigma=0,
+        is_put=False,
+        is_american=False
+    ) -> int:
         """
         Initialise the stock option base class.
         Defaults to European calls unless specified.
         
         Arg:
             S0: Initial stock price
-            strike: Strike price
+            K: Strike price
             r: Risk-free interest rate
             T: Time to maturity
             N: Number of time steps
@@ -24,7 +37,7 @@ class Option(object):
             is_american: True for an American option, False for a European option
         """
         self.S0 = S0
-        self.strike = strike
+        self.K = K
         self.r = r
         self.T = T
         self.N = max(1, N)
@@ -73,9 +86,9 @@ class BinomialEuropean(Option):
         Returns the payoffs when the option expires at terminal nodes
         """
         if self.is_call:
-            return np.maximum(0, self.StockTrees - self.strike)
+            return np.maximum(0, self.StockTrees - self.K)
         else:
-            return np.maximum(0, self.strike - self.StockTrees)
+            return np.maximum(0, self.K - self.StockTrees)
     
     def traverse_tree(self, payoffs):
         """
@@ -101,3 +114,83 @@ class BinomialEuropean(Option):
         
         # Option value converges to first node
         return payoffs[0]
+    
+
+class BinomialTree(Option):
+    """
+    Price a American option by the binomial tree model
+    """
+    def setup_parameters(self):
+        # Required calculations for the model
+        self.u = 1 + self.pu # Expected value in the up state
+        self.d = 1 - self.pd # Expected value in the down state
+        self.qu = (math.exp((self.r - self.div) * self.dt) - self.d)/(self.u - self.d)
+        self.qd = 1 - self.qu
+
+    def init_stock_price_tree(self):
+        # Initialise a 2D tree at T=0
+        self.StockTrees = [np.array([self.S0])]
+        
+        # Simulate the possible stock prices path
+        for i in range(self.N):
+            prev_branches = self.StockTrees[-1]
+            st = np.concatenate((prev_branches*self.u, [prev_branches[-1]*self.d]))
+            self.StockTrees.append(st) # Add nodes at each time step
+    
+    def init_payoffs_tree(self):
+        if self.is_call:
+            return np.maximum(0, self.STs[self.N] - self.K)
+        else:
+            return np.maximum(0, self.K - self.StockTrees[self.N])
+    
+    def check_early_exercise(self, payoffs, node):
+        if self.is_call:
+            return np.maximum(payoffs, self.StockTrees[node] - self.K)
+        else:
+            return np.maximum(payoffs, self.K - self.StockTrees[node])
+    
+    def traverse_tree(self, payoffs):
+        for i in reversed(range(self.N)):
+            # The payoffs from NOT exercising the option
+            payoffs = (payoffs[:-1]*self.qu + payoffs[1:]*self.qd)*self.df
+            
+            # Payoffs from exercising, for American options
+            if not self.is_european:
+                payoffs = self.check_early_exercise(payoffs, i)
+        return payoffs
+
+    def begin_tree_traversal(self):
+        payoffs = self.init_payoffs_tree()
+        return self.traverse_tree(payoffs)
+    
+    def price(self):
+        """
+        Entry point of the pricing implementation
+        """
+        self.setup_parameters()
+        self.init_stock_price_tree()
+        payoffs = self.begin_tree_traversal()
+        
+        # Option value converges to first node
+        return payoffs[0]
+
+class BinomialCRROption(BinomialTree):
+    def setup_parameters(self):
+        self.u = math.exp(self.sigma * math.sqrt(self.dt))
+        self.d = 1/self.u
+        self.qu = (math.exp((self.r - self.div)*self.dt) - self.d)/(self.u - self.d)
+        self.qd = 1 - self.qu
+    
+
+if __name__ == "__main__":
+    eu_option = BinomialEuropean(50, 52, r=0.05, T=2, N=2, pu=0.2, pd=0.2, is_put=True)
+    print(f"European put option price is: {eu_option.price()}")
+    
+    am_option = BinomialTree(50, 52, r=0.05, T=2, N=2, pu=0.2, pd=0.2, is_put=True, is_american=True)
+    print(f"American put option price is: {am_option.price()}")
+    
+    eu_option2 = BinomialCRROption(50, 52, r=0.05, T=2, N=2, sigma=0.3, is_put=True)
+    print(f"European put: {eu_option2.price()}")
+    
+    am_option2 = BinomialCRROption(50, 52, r=0.05, T=2, N=2, sigma=0.3, is_put=True, is_american=True)
+    print(f"American put option price is: {am_option2.price()}")
